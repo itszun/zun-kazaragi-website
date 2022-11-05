0f4b407134a86463931279a03cb554b48dc94ba5
Zun <malikal.j4@gmail.com>
2022-11-05 16:10:31 +0700

    Leftover 2

--- database/seeders/BasicSeeder.php
+++ database/seeders/BasicSeeder.php
@@ -19,23 +19,26 @@ class BasicSeeder extends Seeder
         DB::table('menus')->truncate();
         Menu::insert([[
             'title' => 'Dashboard',
+            'parent_id' => null,
             'url' => "/admin",
             'icon' => "fas fa-fw fa-tachometer-alt",
             'section' => "100"
-        ],[
+        ], [
             'title' => 'Utilities',
             'url' => "/#",
+            'parent_id' => null,
             'icon' => "fas fa-fw fa-wrench",
             'section' => "900"
-        ],[
+        ], [
             'title' => 'Menu',
             'parent_id' => '2',
             'url' => "/admin/menu",
             'icon' => "fas fa-fw fa-tachometer-alt",
             'section' => "900"
-        ],[
+        ], [
             'title' => 'Article',
             'url' => "/admin/article",
+            'parent_id' => null,
             'icon' => "fas fa-fw fa-book",
             'section' => "200"
         ]]);
