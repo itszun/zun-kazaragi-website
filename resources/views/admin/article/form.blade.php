99f9fc98956e4e196fcbdeb49c55b7af740d6958
Zun <malikal.j4@gmail.com>
2022-12-08 19:59:35 +0700

    Installing Orchid

--- resources/views/admin/article/form.blade.php
+++ resources/views/admin/article/form.blade.php
@@ -21,22 +21,32 @@
 
 @section('content')
 <form action="" cemes="form" method="POST" cemes-redirect="{{ url()->previous() }}">
-    <div class="bg-white border">
-        <div class="row my-3">
-            <div class="col-8">
-                <textarea type="text" id="title" name="title" placeholder="Title" class="form-control article-title"
-                >{{ $article->title }}</textarea>
-            </div>
-            <div class="col-4">
-                <div class="float-right p-3">
-                    <a href="{{ route('admin.article.index') }}" class="btn border">Batal</a>
-                    <button type="submit" class="btn btn-primary">Simpan</button>
+    <div class="row">
+        <div class="col-9">
+            <div class="bg-white border">
+                <div class="row my-3">
+                    <div class="col-8">
+                        <textarea type="text" id="title" name="title" placeholder="Title" class="form-control article-title"
+                        >{{ $article->title }}</textarea>
+                    </div>
+                    <div class="col-4">
+                        <div class="float-right p-3">
+                            <a href="{{ route('admin.article.index') }}" class="btn border">Batal</a>
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        </div>
+                    </div>
                 </div>
+                <textarea name="body" id="editor" cols="30" rows="1" class="d-none">
+                    {{ $article->body }}
+                </textarea>
             </div>
         </div>
-        <textarea name="body" id="editor" cols="30" rows="1" class="d-none">
-            {{ $article->body }}
-        </textarea>
+        <div class="col-3">
+            <div class="bg-white">
+                TEst
+            </div>
+        </div>
     </div>
+
 </form>
 @endsection
