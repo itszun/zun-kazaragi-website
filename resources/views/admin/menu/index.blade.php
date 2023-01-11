d2acf5ba2b4b62c66cf2e21e51b9641ee82d3b22
wahyungrho <hallawahyu@gmail.com>
2023-01-11 12:19:02 +0700

    tes pull request

--- resources/views/admin/menu/index.blade.php
+++ resources/views/admin/menu/index.blade.php
@@ -1,26 +1,25 @@
 @extends('admin._template.main')
 
 @section('page_heading_content')
-    <a href="{{ route('admin.menu.create') }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-            class="fas fa-plus-circle fa-sm text-white-50"></i> Tambah Menu </a>
+<a href="{{ route('admin.menu.create') }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+        class="fas fa-plus-circle fa-sm text-white-50"></i> Tambah Menu </a>
 @endsection
 
 
 
 
 @section('content')
-    @include('admin._components.page_heading')
+@include('admin._components.page_heading')
 
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">List Menu</h6>
-        </div>
-        <div class="card-body">
-            <x-datatable.table
-                sourceUrl="{{ PageInfo()->data['datatable_url'] }}"
-                tableHeaders="{{ PageInfo()->data['datatable_headers'] }}"
-                >
-            </x-datatable.table>
-        </div>
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">List Menu</h6>
     </div>
+    <div class="card-body">
+        <x-datatable.table sourceUrl="{{ PageInfo()->data['datatable_url'] }}"
+            tableHeaders="{{ PageInfo()->data['datatable_headers'] }}">
+        </x-datatable.table>
+    </div>
+</div>
+{{-- tes pull request --}}
 @endsection
